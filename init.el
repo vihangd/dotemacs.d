fc8ff3e0ee2362f84955839beee41205b7c5b62c
Vihang D <vihang.dudhalkar@gmail.com>
2014-06-29 08:59:31 +0530

    Updated smart-mode-line

--- init.el
+++ init.el
@@ -2,10 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" "025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "31d3463ee893541ad572c590eb46dcf87103117504099d362eeed1f3347ab18f" "f3d2144fed1adb27794a45e61166e98820ab0bbf3cc7ea708e4bf4b57447ee27" "a2c537c981b4419aa3decac8e565868217fc2995b74e1685c5ff8c6d77b198d6" "31bfef452bee11d19df790b82dea35a3b275142032e06c6ecdc98007bf12466c" "2f80d6ea18d147a6b4e5b54801317b7789531c691edecfa2ab0d2972bee6b36d" default)))
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation)))
+ '(preview-image-type (quote dvipng))
+ '(sml/theme (quote dark)))
+
+
 ;font
 ;(set-frame-font "Inconsolata-14")
 (set-default-font "Source Code Pro-13")
 ;(set-default-font "Dejavu Sans Mono-12")
+
 
 ;; taken from starter-kit
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -176,14 +188,6 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" "025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "31d3463ee893541ad572c590eb46dcf87103117504099d362eeed1f3347ab18f" "f3d2144fed1adb27794a45e61166e98820ab0bbf3cc7ea708e4bf4b57447ee27" "a2c537c981b4419aa3decac8e565868217fc2995b74e1685c5ff8c6d77b198d6" "31bfef452bee11d19df790b82dea35a3b275142032e06c6ecdc98007bf12466c" "2f80d6ea18d147a6b4e5b54801317b7789531c691edecfa2ab0d2972bee6b36d" default)))
- '(haskell-mode-hook (quote (turn-on-haskell-indentation)))
- '(preview-image-type (quote dvipng)))
 
 ;; ensime for scala
 ;; (add-to-list 'load-path "~/.emacs.d/ensime/elisp")
